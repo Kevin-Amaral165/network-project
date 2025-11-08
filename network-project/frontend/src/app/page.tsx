4563a819693058e0d7e3fd2afefa5fad3e590afd
Kevin <kevin_ramos1@hotmail.com>
2025-11-07 23:55:19 -0300

    minor fix

--- network-project/frontend/src/app/page.tsx
+++ network-project/frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-8">Dashboard Principal</h1>
-      <p className="text-lg mb-8">Este é um texto de exemplo no dashboard.</p>
+      <p className="text-lg mb-8">Este é um texto de exemplo no dashboards</p>
       <Button>Clique Aquis</Button>
     </div>
   );
